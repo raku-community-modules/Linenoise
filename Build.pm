0de4a7b975e3f08d1dc568615366c74f62564bec
Rob Hoelz <rob@hoelz.ro>
2015-03-25 07:56:26 -0500

    Revert "Initialize linenoise submodule before building"
    This reverts commit 9d49824f56359315ea9a74c5b5a14c16f746259d.
    
    This doesn't seem to work with panda install git://...

--- Build.pm
+++ Build.pm
@@ -5,8 +5,6 @@ use LibraryMake;
 
 class Build is Panda::Builder {
     method build($workdir) {
-        run('git', 'submodule', 'init');
-        run('git', 'submodule', 'update');
         mkdir("$workdir/blib");
         mkdir("$workdir/blib/lib");
         make($workdir, "$workdir/blib/lib");
