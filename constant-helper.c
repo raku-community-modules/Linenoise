f368fe0d6c31f05f69d9801c7c52b93ebec92eaa
Rob Hoelz <rob@hoelz.ro>
2015-11-21 12:36:50 -0600

    Use constant helper to write out the stdlib library name
    This is for Windows, which needs a library name to link against
    functions in the C standard library.  Non-VC++ compilers use
    msvcrt.dll, whereas VC++ compilers use a version-specific suffix
    that amounts to (_MSC_VER / 10) - 60 (except for versions between 4.0
    and 7.0, which use msvcrt.dll)

--- constant-helper.c
+++ constant-helper.c
@@ -7,6 +7,17 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
+
+static int
+msc_version_to_dll_version(int msc_version)
+{
+    if(msc_version == 800) {
+        return 10; /* 1.0 is an exception */
+    } else {
+        return (msc_version / 10) - 60;
+    }
+}
 
 int
 main(void)
@@ -14,6 +25,16 @@ main(void)
     printf("F_GETFL=%d\n", F_GETFL);
     printf("F_SETFL=%d\n", F_SETFL);
     printf("O_NONBLOCK=%d\n", O_NONBLOCK);
+
+#if _WIN32 || _WIN64
+# if defined(_MSC_VER) && (_MSC_VER < 1000 || _MSC_VER >= 1300)
+    printf("CLIB=\"msvcr%d.dll\"\n", msc_version_to_dll_version(_MSC_VER));
+# else
+    printf("CLIB=\"msvcrt.dll\"\n");
+# endif
+#else
+    printf("CLIB=Str\n");
+#endif
 
     return 0;
 }
