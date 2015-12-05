9f79f92bccd8a1f95373696a81b84d05a12940b1
Rob Hoelz <rob@hoelz.ro>
2015-12-04 18:38:10 -0600

    Install liblinenoise.so into installation lib directory by hand
    Until %?RESOURCES is implemented, this is a quick and dirty way to get
    Linenoise working again

--- Build.pm
+++ Build.pm
@@ -7,6 +7,10 @@ class Build is Panda::Builder {
     method build($workdir) {
         mkdir("$workdir/blib");
         mkdir("$workdir/blib/lib");
-        make($workdir, "$workdir/blib/lib");
+        my %vars = get-vars("$workdir/blib/lib");
+        %vars<PREFIX> = $*VM.config<prefix>;
+
+        process-makefile($workdir, %vars);
+        shell(%vars<MAKE>);
     }
 }
