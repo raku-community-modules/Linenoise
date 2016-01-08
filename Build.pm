a6f05b98f35092b2197d99e5c48bfb3fc3ca1a31
Rob Hoelz <rob@hoelz.ro>
2016-01-07 22:04:33 -0600

    Convert to use Native::Resources

--- Build.pm
+++ Build.pm
@@ -1,23 +1,10 @@
 use v6;
 
 use Panda::Builder;
-use LibraryMake;
+use Native::Resources::Build;
 
 class Build is Panda::Builder {
     method build($workdir) {
-        mkdir("$workdir/blib");
-        mkdir("$workdir/blib/lib");
-        my %vars = get-vars("$workdir/blib/lib");
-        my @shared-object-extensions = <.so .dll .dylib>.grep(* ne %vars<SO>);
-
-        %vars<FAKESO> = @shared-object-extensions.map('resources/lib/liblinenoise' ~ *);
-
-        my $fake-so-rules = @shared-object-extensions.map({
-            "resources/lib/liblinenoise$_:\n\tperl6 -e \"print ''\" > resources/lib/liblinenoise$_"
-        }).join("\n");
-
-        process-makefile($workdir, %vars);
-        spurt("$workdir/Makefile", $fake-so-rules, :append);
-        shell(%vars<MAKE>);
+        make($workdir, "$workdir/resources/lib", :libname<linenoise>);
     }
 }
