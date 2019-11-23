85d564c540c417340c8312e67a0b23d62e610020
Sizhe Zhao <prc.zhao@outlook.com>
2019-11-23 09:32:03 +0800

    fix

--- fill-constants.pl
+++ fill-constants.pl
@@ -23,8 +23,9 @@ sub MAIN {
                 $new-line.substr-rw($<value>.from, $<value>.chars) = '0x%x'.sprintf(%constants{$ident});
             }
         }
-        elsif $line.contains('#`(FILL-THIS)') {
-            $new-line = $new-line.subst('#`(FILL-THIS)',$*DISTRO.is-win ?? ("'" ~ CompUnit::RepositoryRegistry.repository-for-name("site").Str ~ "'") !! "''");
+        elsif $line.contains('#`(FILL-THIS') {
+            $new-line = $new-line.subst('#`(FILL-THIS-1)',$*DISTRO.is-win.Str);
+            $new-line = $new-line.subst('#`(FILL-THIS-2)',$*DISTRO.is-win ?? ("'" ~ CompUnit::RepositoryRegistry.repository-for-name("site").Str ~ "'") !! "''");
         }
         say $new-line;
     }
