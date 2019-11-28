1c56fe2c4e0655991c0bf34c8e2cb405f477d7ac
JJ Merelo <jjmerelo@gmail.com>
2019-11-28 17:59:41 +0100

    Using OS-independent IO::Path

--- Build.pm
+++ Build.pm
@@ -5,7 +5,8 @@ class Build {
     method build($srcdir) {
         my %vars = get-vars($srcdir);
         %vars<linenoise> = $*VM.platform-library-name('linenoise'.IO).basename;
-        mkdir "$srcdir/resources/libraries" unless "$srcdir/resources/libraries".IO.e;
+        say "Creating directory";
+        mkdir IO::Path.new("$srcdir/resources/libraries") unless "$srcdir/resources/libraries".IO.e;
         say "Processing makefile in $srcdir with %vars";
         process-makefile($srcdir, %vars);
         shell %vars<MAKE>, :cwd($srcdir);
