95abe86821153095e2fa8ba2e0454fb860d070cd
JJ Merelo <jjmerelo@gmail.com>
2019-11-28 18:09:52 +0100

    Trying to see something

--- Build.pm
+++ Build.pm
@@ -5,9 +5,7 @@ class Build {
     method build($srcdir) {
         my %vars = get-vars($srcdir);
         %vars<linenoise> = $*VM.platform-library-name('linenoise'.IO).basename;
-        say "Creating directory";
         mkdir IO::Path.new("$srcdir/resources/libraries") unless "$srcdir/resources/libraries".IO.e;
-        say "Processing makefile in $srcdir with %vars";
         process-makefile($srcdir, %vars);
         shell %vars<MAKE>, :cwd($srcdir);
     }
